3079bf17b21e04cdad74d3905dd56e69c11be32c
Camof1ow <astk1203@nate.com>
2022-08-23 23:44:41 +0900

    CodeDeploy용 shell 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,10 @@
 
 PROJECT_NAME="foocebak"
 JAR_PATH="/home/ubuntu/app/$PROJECT_NAME/build/libs/*.jar"
-DEPLOY_PATH=/home/app/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/app/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/app/$PROJECT_NAME/deploy_err.log"
 APPLICATION_LOG_PATH="/home/app/$PROJECT_NAME/application.log"
 BUILD_JAR=$(ls $JAR_PATH)
-JAR_NAME=$(basename $BUILD_JAR)
 
 echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
@@ -24,12 +22,12 @@ else
   echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
   echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
   echo "> kill -9 $CURRENT_PID" >> $DEPLOY_LOG_PATH
-  kill -9 $CURRENT_PID
+  kill $(lsof -t -i:8080)
 fi
 
 
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-nohup java -jar clone-project-0.0.1-SNAPSHOT.jar --spring.config.location=./application.properties >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+nohup java -jar clone-project-0.0.1-SNAPSHOT.jar --spring.config.location=./application.properties --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 sleep 3
 
